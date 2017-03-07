4e3a64e5057bf181b7048932afa272ed1426c991
OsamaFathy <osama.fathy@hotmail.com>
2017-03-07 18:52:33 +0200

    Add Optimizer, BaseSGDOptimizer and MomentumOptimizer types

--- trainer_config_helpers/optimizers.jl
+++ trainer_config_helpers/optimizers.jl
@@ -1,1 +1,48 @@
 # optimizers file
+abstract Optimizer
+
+abstract BaseSGDOptimizer <: Optimizer
+
+type MomentumOptimizer <: BaseSGDOptimizer
+    extra_settings::Function
+    to_setting_kwargs::Function
+    #TODO is_support_sparse::Function
+    #TODO explicitely state types?
+    momentum
+    sparse
+
+    function MomentumOptimizer(momentum = nothing, sparse = false)
+        this = new()
+        
+        this.momentum = momentum
+        this.sparse = sparse
+
+        this.extra_settings = function ()
+            #TODO call default_momentum function in config_parser file
+        end
+
+        this.to_setting_kwargs = function ()
+            if sparse
+                Dict("learning_method" => "sparse_momentum")
+            else
+                Dict("learning_method" => "momentum")
+            end
+        end
+
+        return this
+    end
+end
+
+#TODO delete this
+function momentumoptimizer_basic_tests()
+    x = MomentumOptimizer(10, true)
+    println("momentum: $(x.momentum), sparse: $(x.sparse)")
+    x.extra_settings()
+    println(x.to_setting_kwargs()["learning_method"])
+
+    x = MomentumOptimizer()
+    println("momentum: $(x.momentum), sparse: $(x.sparse)")
+    println(x.to_setting_kwargs()["learning_method"])
+end
+
+basic_tests()
