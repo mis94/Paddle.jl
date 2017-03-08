9623a188a9c8268dd55a86043572e14fe1ef0de8
mis94 <mis1494@gmail.com>
2017-03-08 02:47:10 +0200

    started LayerType class

--- trainer_config_helpers/layers.jl
+++ trainer_config_helpers/layers.jl
@@ -1,1 +1,26 @@
 # layers file
+
+type LayerType
+    # layer type enumerations
+    DATA::AbstractString
+    is_layer_type::Function
+
+    function LayerType()
+        this = new()
+
+        this.DATA = "data"
+
+        this.is_layer_type = function(type_name)
+            println(type_name)
+        end
+
+        return this
+    end
+end
+
+
+layerType = LayerType()
+
+#println(typeof(layerType))
+#println(layerType.DATA)
+#layerType.is_layer_type("I hope this is printed")
