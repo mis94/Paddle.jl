e5b982f0a80ede898b08b9d0590fe86a18f0a3cb
OsamaFathy <osama.fathy@hotmail.com>
2017-03-14 16:03:18 +0200

    Add AdamOptimizer

--- trainer_config_helpers/optimizers.jl
+++ trainer_config_helpers/optimizers.jl
@@ -4,12 +4,13 @@ abstract Optimizer
 abstract BaseSGDOptimizer <: Optimizer
 
 type MomentumOptimizer <: BaseSGDOptimizer
+    momentum
+    sparse
     extra_settings::Function
     to_setting_kwargs::Function
     #TODO is_support_sparse::Function
     #TODO explicitely state types?
-    momentum
-    sparse
+    
 
     function MomentumOptimizer(momentum = nothing, sparse = false)
         this = new()
@@ -33,8 +34,36 @@ type MomentumOptimizer <: BaseSGDOptimizer
     end
 end
 
+
+type AdamOptimizer <: BaseSGDOptimizer
+    beta1
+    beta2
+    epsilon
+    extra_settings::Function
+    to_setting_kwargs::Function
+    #TODO is_support_sparse::Function
+    function AdamOptimizer(beta1=0.9, beta2=0.999, epsilon=1e-8)
+        this = new()
+        this.beta1 = beta1
+        this.beta2 = beta2
+        this.epsilon = epsilon
+        this.extra_settings = function ()
+        end
+
+        this.to_setting_kwargs = function()
+            return Dict("learning_method" => "adam",
+                        "adam_beta1" => this.beta1,
+                        "adam_beta2" => this.beta2,
+                        "adam_epsilon" => this.epsilon)
+        end
+    
+        return this
+    end
+end
+
 #TODO delete this
-function momentumoptimizer_basic_tests()
+function basic_tests()
+    # ================= MomentumOptimizer =================
     x = MomentumOptimizer(10, true)
     println("momentum: $(x.momentum), sparse: $(x.sparse)")
     x.extra_settings()
@@ -43,6 +72,21 @@ function momentumoptimizer_basic_tests()
     x = MomentumOptimizer()
     println("momentum: $(x.momentum), sparse: $(x.sparse)")
     println(x.to_setting_kwargs()["learning_method"])
+
+    # ================= AdamOptimizer =================
+    x = AdamOptimizer(10, 20, 30);
+    println("beta1: $(x.beta1), beta2: $(x.beta2), epsilon: $(x.epsilon)")
+    x.extra_settings()
+    y = x.to_setting_kwargs()
+    println(y["learning_method"])
+
+    x = AdamOptimizer()
+    println("beta1: $(x.beta1), beta2: $(x.beta2), epsilon: $(x.epsilon)")
+    x.extra_settings()
+    y = x.to_setting_kwargs()
+    println(y["learning_method"])
+
+    
 end
 
 basic_tests()
