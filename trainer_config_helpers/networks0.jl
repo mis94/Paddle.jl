080173d08c3447ce4d66e2fdd06d06008bd237b2
Kareem <kimo.h2010@gmail.com>
2017-04-16 10:40:07 +0200

    used isa() instead of typeof()

--- trainer_config_helpers/networks0.jl
+++ trainer_config_helpers/networks0.jl
@@ -7,21 +7,22 @@ the data provider's return order.
 :return:
 """
 function inputs(layers, args...)
-  if isa(layers, LayerOutput) || isa(layer, String)
+  # This is not working :/
+  # if is(layers, LayerOutput) || is(layers, basestring)
+  #   layers = [layers]
+  # end
+  if typeof(layers) == LayerOutput || typeof(layer) == String
     layers = [layers]
   end
-
   if legnth(args) != 0
     for arg in args
       layers = vcat(layers, arg)
     end
   end
-
   names = []
   for layer in layers
     names = vcat(names, layer.name)
   end
-
   Inputs(names)
 end
 
@@ -37,31 +38,26 @@ network, this method will calculate the input order by dfs travel.
 :return:
 """
 function outputs(layers, args...)
-  function __dfs_travel__(layer, predicate = x -> x.layer_type == "DATA") #TODO: Change hardcoded "DATA" to LayerType.DATA
-    @assert isa(layer, LayerOutput) @sprintf("layer is %s", layer)
+  function __dfs_travel__(layer, predicate = x -> x.layer_type == LayerType.DATA)
+    @assert typeof(layer)==LayerOutput @sprintf("layer is %s", layer)
     retv = []
-
     if layer.parents != nothing
       for p in layer.parents
         retv = vcat(__dfs_travel__(p, predicate))
       end
     end
-
     if predicate(layer)
       retv = vcat(retv, layer)
     end
-
     return retv
   end
 
-  if isa(layers, LayerOutput)
+  if typeof(layers) == LayerOutput
     layers = [layers]
   end
-
   if length(args) != 0
     layers = vcat(layers, args)
   end
-
   @assert length(layers) > 0
 
   if HasInputsSet()
@@ -71,44 +67,40 @@ function outputs(layers, args...)
     end
     return
   end
-
-  # if length(layers) != 1
-  #   logger.warning("`outputs` routine try to calculate network's, inputs and outputs order. It might not work well. Please see follow log carefully.")
-  # end
-
+  if length(layers) != 1
+    logger.warning("`outputs` routine try to calculate network's, inputs and outputs order. It might not work well. Please see follow log carefully.")
+  end
   inputs = []
   outputs = []
-
   for each_layer in layers
-    @assert isa(each_layer, LayerOutput)
+    @assert typeof(each_layer) == LayerOutput
     inputs = vcat(inputs, __dfs_travel__(each_layer))
-    outputs = vcat(outputs, __dfs_travel__(each_layer, x -> x.layer_type == "COST")) #TODO: Change hardcoded "COST" to LayerType.COST
+    outputs = vcat(outputs, __dfs_travel__(each_layer, x -> x.layer_type==LayerType.COST))
   end
-
   final_inputs = []
-  final_outputs =
+  final_outputs = []
 
   for each_input in inputs
-    @assert isa(each_input, LayerOutput)
+    @assert typeof(each_input) == LayerOutput
     if !(each_input.name in final_inputs)
       final_inputs = vcat(final_inputs, each_input.name)
     end
   end
 
   for each_out in outputs
-    @assert isa(each_out, LayerOutput)
+    @assert typeof(each_out) == LayerOutput
     if !(each_output.name in final_outputs)
       final_outputs = vcat(final_outputs, each_output.name)
     end
   end
 
-  # logger.info("The input order is [" * join(final_inputs, ", ") * "]")
+  logger.info("The input order is [" * join(final_inputs, ", ") * "]")
 
   if length(final_outputs) == 0
     final_outputs = map(x->x.name, layers)
   end
 
-  # logger.info("The output order is [" * join(final_outputs, ", ") * "]")
+  logger.info("The output order is [" * join(final_outputs, ", ") * "]")
 
   Inputs(final_inputs)
   Outputs(final_outputs)
