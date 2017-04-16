91615d7a26bcab90cb059f4b58bfe48abbcb0d9c
Kareem <kimo.h2010@gmail.com>
2017-04-16 14:51:23 +0200

    added config_parse.jl, Inputs, Outputs, HasInputsSets

--- trainer/config_parser.jl
+++ trainer/config_parser.jl
@@ -1,43 +1,61 @@
-# # functions available in config file
-#
-#
-# # Define the name of the input layers of the NeuralNetwork.
-# # The type of these layers must be "data".
-# # These layers will be provided with the DataBatch obtained
-# # from DataProvider. The data streams from DataProvider must
-# # have the same order.
-# @config_func
-# def Inputs(*args):
-#     for name in args:
-#         name = MakeLayerNameInSubmodel(name)
-#         global g_current_submodel, g_root_submodel
-#         if g_current_submodel.is_recurrent_layer_group:
-#             config_assert(False, "Do not set Inputs in recurrent layer group")
-#         else:
-#             g_current_submodel.input_layer_names.append(name)
-#
-#         if g_current_submodel is g_root_submodel:
-#             g_config.model_config.input_layer_names.append(name)
-#
-#
-# @config_func
-# def HasInputsSet():
-#     return len(g_current_submodel.input_layer_names) != 0
-#
-#
-# # Define the name of the output layers of the NeuralNetwork.
-# # Usually the output is simply the cost layer.
-# # You can specify other layers as outputs and calculate the
-# # cost (and its derivative) yourself.
-# @config_func
-# def Outputs(*args):
-#     for name in args:
-#         name = MakeLayerNameInSubmodel(name)
-#         global g_current_submodel, g_root_submodel
-#         if g_current_submodel.is_recurrent_layer_group:
-#             config_assert(False, "Do not set Outputs in recurrent layer group")
-#         else:
-#             g_current_submodel.output_layer_names.append(name)
-#
-#         if g_current_submodel is g_root_submodel:
-#             g_config.model_config.output_layer_names.append(name)
+include(dirname(Base.source_path()) * "/globals.jl")
+using globals
+
+
+function NoDecoInputs(args)
+  for name in args
+
+    name = MakeLayerNameInSubmodel(name)
+    if globals.g_current_submodel.is_recurrent_layer_group
+      config_assert(false, "Do not set Inputs in recurrent layer group")
+    else
+      append!(globals.g_current_submodel.input_layer_names, name)
+    end
+
+    if is(globals.g_current_submodel, globals.g_root_submodel)
+      append!(globals.g_config.model_config.input_layer_names, name)
+    end
+
+  end
+end
+
+
+function Inputs()
+  deco_config_func(NoDecoInputs, "Inputs")
+end
+
+
+function deco_config_func(func, name)
+  globals.g_config_funcs[name] = func
+  return func
+end
+
+
+function NoDecoHasInputsSet()
+  length(globals.g_current_submodel.input_layer_name) != 0
+end
+
+function HasInputsSet()
+  deco_config_func(NoDecoHasInputsSet, "HasInputsSet")
+end
+
+function NoDecoOutputs(args)
+  for name in args
+
+    name = MakeLayerNameInSubmodel(name)
+    if globals.g_current_submodel.is_recurrent_layer_group
+      config_assert(false, "Do not set Outputs in recurrent layer group")
+    else
+      append!(globals.g_current_submodel.output_layer_names, name)
+    end
+
+    if is(globals.g_current_submodel, globals.g_root_submodel)
+      append!(globals.g_config.model_config.output_layer_names, name)
+    end
+
+  end
+end
+
+function Outputs()
+  deco_config_func(NoDecoOutputs, "Outputs")
+end
