5cbfff8e3e6f7a7a65e6d5748d46f29cd763dacb
mis94 <mis1494@gmail.com>
2017-04-19 13:54:34 +0200

    Eliminated unnecessary decorators

--- trainer/config_parser.jl
+++ trainer/config_parser.jl
@@ -23,24 +23,8 @@ function config_assert(b, msg)
   end
 end
 
-function deco_config_func(func, name)
-  globals.g_config_funcs[name] = func
-end
-
-function deco_config_func1(func, name, args)
-  deco_config_func(func, name)
-  if(is(args, nothing))
-    return func()
-  end
-  return func(args)
-end
-
-function deco_config_func2(func, name, layerName, layerType, kwargs)
-  deco_config_func(func, name)
-  func(layerName, layerType, kwargs)
-end
-
-function NoDecoInputs(args)
+function Inputs(args)
+  globals.g_config_funcs[string(Inputs)] = Inputs
   for name in args
 
     name = MakeLayerNameInSubmodel(name)
@@ -57,19 +41,13 @@ function NoDecoInputs(args)
   end
 end
 
-function Inputs(args)
-  deco_config_func1(NoDecoInputs, string(Inputs), args)
-end
-
-function NoDecoHasInputsSet()
+function HasInputsSet()
+  globals.g_config_funcs[string(HasInputsSet)] = HasInputsSet
   length(globals.g_current_submodel.input_layer_name) != 0
 end
 
-function HasInputsSet()
-  deco_config_func1(NoDecoHasInputsSet, string(HasInputsSet), nothing)
-end
-
-function NoDecoOutputs(args)
+function Outputs(args)
+  globals.g_config_funcs[string(Outputs)] = Outputs
   for name in args
 
     name = MakeLayerNameInSubmodel(name)
@@ -86,20 +64,13 @@ function NoDecoOutputs(args)
   end
 end
 
-function Outputs(args)
-  deco_config_func1(NoDecoOutputs, string(Outputs), args)
-end
-
-function NoDecoLayer(name, layerType, kwargs)
+function Layer(name, layerType, kwargs)
+  globals.g_config_funcs[string(Layer)] = Layer
   layers = Dict()
   merge!(layers, globals.g_cost_map, globals.g_layer_type_map)
   layer_func = layers[layerType]
   config_assert(layer_func, "layer type " * layerType * " not supported")
   return layer_func(name, kwargs)
-end
-
-function Layer(name, layerType, kwargs)
-  deco_config_func2(NoDecoLayer, string(Layer), name, layerType, kwargs)
 end
 
 type Bias
