0d747340a9b7b58b9f1e360d8286a5af924fa909
mis94 <mis1494@gmail.com>
2017-06-13 18:45:12 +0200

    Added configuration file

---
+++ trainer_config_helpers/trainer_config.lr.jl
@@ -0,0 +1,8 @@
+include("../trainer/config_parser.jl")
+include("data_sources.jl")
+include("optimizers.jl")
+include("layers.jl")
+include("activations.jl")
+include("networks.jl")
+
+#This file should be implemented and we can test the API by running it
